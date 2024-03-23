24e9f3883e507b5955de43a246ad2f0cade7f007
JoelBJoseph <joelbjoseph03@gmail.com>
2024-03-23 07:47:46 +0530

    nav begin

--- component/Navabar.jsx
+++ component/Navabar.jsx
@@ -1,39 +1,13 @@
 "use client";
 
-import Link from "next/link";
-import {useRouter} from "next/router";
-
 const Navabar = () => {
-
-    const router = useRouter([])
-
     return (
 
-        <nav>
-            <ul>
-                <li>
-                    <Link href="/" passHref>
-                        <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="./Home/about" passHref>
-                        <a className={router.pathname === '/Home/about' ? 'active' : ''}>About</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/Home/services" passHref>
-                        <a className={router.pathname === '/Home/services' ? 'active' : ''}>Services</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/Home/contact" passHref>
-                        <a className={router.pathname === '/Home/contact' ? 'active' : ''}>Services</a>
-                    </Link>
-                </li>
-                {/* Add more menu items as needed */}
-            </ul>
-        </nav>
+        <div>
+            <nav>
+                <></>
+            </nav>
+        </div>
 
     );
 }
